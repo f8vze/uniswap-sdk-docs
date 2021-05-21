c2d0c4bee6739960e2293e7d30d337fe8e9aa954
Connor Martin <cnr.mrtn@gmail.com>
2021-05-21 08:42:28 -0400

    config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   title: "Uniswap",
-  tagline: "Documentation and Guides",
-  url: "https://docs.uniswap.org/",
+  tagline: "SDK Documentation",
+  url: "https://docs.sdk.uniswap.org/",
   baseUrl: "/",
   onBrokenLinks: "ignore",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.png",
   organizationName: "Uniswap", // Usually your GitHub org/user name.
-  projectName: "Uniswap-docs", // Usually your repo name.
+  projectName: "Uniswap-sdk-docs", // Usually your repo name.
   themeConfig: {
    // hideableSidebar: true,
     prism: {
@@ -15,7 +15,7 @@ module.exports = {
     },
     algolia: {
       apiKey: "32465e2ab6f7554ff014e64c0d92171c",
-      indexName: "v3-docs",
+      indexName: "v3-sdk-docs",
       appId: "S0IDD0YGLZ"
     },
     navbar: {
@@ -47,13 +47,13 @@ module.exports = {
           routeBasePath: "/",
           sidebarPath: require.resolve('./sidebars.js'),
           includeCurrentVersion: true,
-          editUrl: "https://github.com/uniswap/uniswap-docs/tree/main/",
+          editUrl: "https://github.com/uniswap/uniswap-sdk-docs/tree/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+          "https://github.com/uniswap/uniswap-sdk-docs/tree/main/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
